5bb0a17dbe63965ca3ad169617c229212e2f2c8e
Aries Wibowo <riespratama@gmail.com>
2025-02-13 16:41:39 +0700

    Testing Kaiadmin

--- PR_App/forms.py
+++ PR_App/forms.py
@@ -5,7 +5,20 @@ from .models import PR_Document, PR_Document_Detail
 class PRDocumentForm(forms.ModelForm):
     class Meta:
         model = PR_Document
-        fields = ['nomor_pr', 'acknowledged_by', 'approved_by', 'director_apprv_by', 'received_by']
+        fields = ['nomor_pr', 'created_by']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Tambahkan class CSS ke field nomor_pr
+        self.fields['nomor_pr'].widget.attrs.update({
+            'class': 'form-control',  # Class Bootstrap untuk input
+            'placeholder': 'Enter PR Number',  # Placeholder opsional
+        })
+        
+        self.fields['created_by'].widget.attrs.update({
+            'class': 'form-control',
+            'disabled': True,  # Membuat field read-only
+        })
 
 class PRDocumentDetailForm(forms.ModelForm):
     class Meta:
@@ -17,6 +30,6 @@ PRDocumentDetailFormSet = inlineformset_factory(
     PR_Document,  # Model induk
     PR_Document_Detail,  # Model anak
     form=PRDocumentDetailForm,  # Form untuk model anak
-    extra=1,  # Jumlah form kosong yang ditampilkan
+    extra=5,  # Jumlah form kosong yang ditampilkan
     can_delete=True,  # Memungkinkan penghapusan detail
 )
--- PR_App/views.py
+++ PR_App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PRDocumentForm, PRDocumentDetailFormSet
+from django.utils import timezone
 
 #@login_required
 def create_pr_document(request):
@@ -14,6 +15,7 @@ def create_pr_document(request):
             # Simpan PR_Document
             pr_document = pr_form.save(commit=False)
             pr_document.created_by = request.user  # Set created_by ke user yang sedang login
+            pr_document.created_date = timezone.now().date()  # Set created_date ke tanggal saat ini
             pr_document.save()
 
             # Simpan PR_Document_Detail
@@ -27,7 +29,10 @@ def create_pr_document(request):
         pr_form = PRDocumentForm()
         detail_formset = PRDocumentDetailFormSet()
 
+    # Tambahkan created_date ke context untuk ditampilkan di template
+    created_date = timezone.now().date()
+
     return render(request, 'create_pr_document.html', {
-        'pr_form': pr_form,
+        'pr_form': pr_form, 'created_date': created_date,
         'detail_formset': detail_formset,
     })
